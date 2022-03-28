78b5bd01a6ee7f589d63dd8c96e00e001be64674
Manoj Kumar K S <98869086+roguepirate13@users.noreply.github.com>
2022-03-28 17:39:28 +0530

    Add files via upload

---
+++ string&dict_op.py
@@ -0,0 +1,13 @@
+text = input("Please enter a string:")
+dictionary = {}
+for keys in text:
+    dictionary[keys] = dictionary.get(keys, 0) + 1
+l1 = list(dictionary)
+result = []
+for key in dictionary:
+    result.append((dictionary[key], key))
+    result.sort(reverse=True)
+for count, letter in result:
+    print(letter, count)
+
+'''Took almost a day to crack this'''
