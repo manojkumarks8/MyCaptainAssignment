1bdafe1f2a77a5719496813b402033d0ab33c37e
Manoj Kumar K S <98869086+roguepirate13@users.noreply.github.com>
2022-03-28 16:08:41 +0530

    Add files via upload

---
+++ list_op.py
@@ -0,0 +1,13 @@
+l1 = [12, -7, 5, 64, -14]
+l2 = [12, 14, -95, 3]
+for num in l1:
+    if num < 0:
+        l1.remove(num)
+print(l1)
+for ele in l2:
+    if ele < 0:
+        l2.remove(ele)
+print(l2)
+
+
+
